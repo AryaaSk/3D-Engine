74607f54b60a90e52d914e0019ec3fcc926467a9
AryaaSk <aryaa_saravana@outlook.com>
2022-03-30 22:12:55 +0100

    Deleted old commented code

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-"use strict";
 //TO ENABLE AUTO RELOAD, RUN LIVE-SERVER, AND CLICK CMD+SHIFT+B, THEN CLICK WATCH TSC: WATCH
-const dpi = window.devicePixelRatio;
-const canvas = document.getElementById('renderingWindow');
-const c = canvas.getContext('2d');
-const canvasHeight = document.getElementById('renderingWindow').getBoundingClientRect().height; //Fix blury lines
-const canvasWidth = document.getElementById('renderingWindow').getBoundingClientRect().width;
+var dpi = window.devicePixelRatio;
+var canvas = document.getElementById('renderingWindow');
+var c = canvas.getContext('2d');
+var canvasHeight = document.getElementById('renderingWindow').getBoundingClientRect().height; //Fix blury lines
+var canvasWidth = document.getElementById('renderingWindow').getBoundingClientRect().width;
 canvas.setAttribute('height', String(canvasHeight * dpi));
 canvas.setAttribute('width', String(canvasWidth * dpi));
 //ACTUAL DRAWING FUNCTIONS
-const gridX = (x) => {
+var gridX = function (x) {
     return (canvasWidth / 2) + x;
 };
-const gridY = (y) => {
+var gridY = function (y) {
     return (canvasHeight / 2) - y;
 };
-const drawLine = (p1, p2) => {
+var drawLine = function (p1, p2) {
     //points will be in format: [x, y]
     //I need to convert the javascript x and y into actual grid x and y
     c.beginPath();
@@ -22,44 +21,46 @@ const drawLine = (p1, p2) => {
     c.lineTo(gridX(p2[0] * dpi), gridY(p2[1] * dpi));
     c.stroke();
 };
-const plotPoint = (p) => {
+var plotPoint = function (p) {
     //point will be in format: [x, y]
     c.fillRect(gridX(p[0] * dpi), gridY(p[1] * dpi), 10, 10);
 };
 //MATRIX FUNCTIONS
-class matrix {
-    /* The data will be stored like on the left, on the right is how the actual matrix will look if you wrote it in mathmatics
-   [[1, 0],          [ [1   [0   [0
-    [0, 1]      =       0],  1],  0] ]
-    [0, 0]]
-    */
-    data = []; /* DO NOT SET THIS EXPLICITLY, USE THE FUNCTIONS */
-    width = 0; //num of columns
-    height = 0; //num of rows
-    addColumn(nums) {
+var matrix = /** @class */ (function () {
+    function matrix() {
+        /* The data will be stored like on the left, on the right is how the actual matrix will look if you wrote it in mathmatics
+       [[1, 0],          [ [1   [0   [0
+        [0, 1]      =       0],  1],  0] ]
+        [0, 0]]
+        */
+        this.data = []; /* DO NOT SET THIS EXPLICITLY, USE THE FUNCTIONS */
+        this.width = 0; //num of columns
+        this.height = 0; //num of rows
+    }
+    matrix.prototype.addColumn = function (nums) {
         this.data.push(nums);
         this.height = nums.length;
         this.width += 1;
-    }
-    addRow(nums) {
+    };
+    matrix.prototype.addRow = function (nums) {
         //to add a row you just need to add the given nums to the end of each column, we first need to check that nums == width
         if (nums.length != this.width) {
             console.error("Unable to add row since length of inputs is not equal to number of columns");
             return;
         }
-        for (let i in nums) {
+        for (var i in nums) {
             this.data[i].push(nums[i]);
             i += 1;
         }
         this.height += 1;
-    }
-    printMatrix() {
+    };
+    matrix.prototype.printMatrix = function () {
         //loop through the rows, and inside of that loop, loop through all the columns
-        let finalOutput = "Matrix:";
-        let currentRow = 0;
+        var finalOutput = "Matrix:";
+        var currentRow = 0;
         while (currentRow != this.height) {
-            let currentLineOutput = "\n";
-            let currentColumn = 0;
+            var currentLineOutput = "\n";
+            var currentColumn = 0;
             while (currentColumn != this.width) {
                 currentLineOutput = currentLineOutput + (this.data[currentColumn][currentRow]) + "      ";
                 currentColumn += 1;
@@ -68,42 +69,42 @@ class matrix {
             currentRow += 1;
         }
         console.log(finalOutput);
-    }
-    getColumn(columnIndex) { return this.data[columnIndex]; }
-    getRow(rowIndex) {
-        let returnArray = [];
-        for (let i in this.data) {
+    };
+    matrix.prototype.getColumn = function (columnIndex) { return this.data[columnIndex]; };
+    matrix.prototype.getRow = function (rowIndex) {
+        var returnArray = [];
+        for (var i in this.data) {
             returnArray.push(this.data[i][rowIndex]);
         }
         return returnArray;
-    }
-    setValue(columnIndex, rowIndex, value) { this.data[columnIndex][rowIndex] = value; }
-    getValue(columnIndex, rowIndex) { return this.data[columnIndex][rowIndex]; }
-    constructor() { }
+    };
+    matrix.prototype.setValue = function (columnIndex, rowIndex, value) { this.data[columnIndex][rowIndex] = value; };
+    matrix.prototype.getValue = function (columnIndex, rowIndex) { return this.data[columnIndex][rowIndex]; };
     ;
-}
-const multiplyMatrixs = (m1, m2) => {
+    return matrix;
+}());
+var multiplyMatrixs = function (m1, m2) {
     //check that m1.width == m2.height, the result matrix will be m1.height x m2.width
     //create result matrix:
-    const resultMatrix = new matrix();
-    const rMatrixHeight = m1.height;
-    const rMatrixWidth = m2.width;
-    for (let _ = 0; _ != rMatrixWidth; _ += 1) {
-        const newColumn = [];
-        for (let __ = 0; __ != rMatrixHeight; __ += 1) {
+    var resultMatrix = new matrix();
+    var rMatrixHeight = m1.height;
+    var rMatrixWidth = m2.width;
+    for (var _ = 0; _ != rMatrixWidth; _ += 1) {
+        var newColumn = [];
+        for (var __ = 0; __ != rMatrixHeight; __ += 1) {
             newColumn.push(0);
         }
         resultMatrix.addColumn(newColumn);
     }
     //now loop through each element in the result matrix with the rowIndex and columnIndex, and calculate it
-    let columnIndex = 0;
+    var columnIndex = 0;
     while (columnIndex != resultMatrix.width) {
-        let rowIndex = 0;
+        var rowIndex = 0;
         while (rowIndex != resultMatrix.height) {
-            const currentRow = m1.getRow(rowIndex);
-            const currentColumn = m2.getColumn(columnIndex); //these 2 should be the same length
-            let value = 0;
-            let i = 0;
+            var currentRow = m1.getRow(rowIndex);
+            var currentColumn = m2.getColumn(columnIndex); //these 2 should be the same length
+            var value = 0;
+            var i = 0;
             while (i != currentRow.length) {
                 value += currentRow[i] * currentColumn[i];
                 i += 1;
@@ -118,15 +119,15 @@ const multiplyMatrixs = (m1, m2) => {
 //RENDERING AN OBJECT
 //first we need to define our transformation matrix, iHat = x axis, jHat = y axis, kHat = z axis, these are vectors
 //            x, y  (Physical grid)
-const iHat = [1, 0];
-const jHat = [0, 0.7];
-const kHat = [0, 0.3];
-const tMatrix = new matrix(); //transformation matrix, cube pointing slightly forward
+var iHat = [1, 0];
+var jHat = [0, 0.7];
+var kHat = [0, 0.3];
+var tMatrix = new matrix(); //transformation matrix, cube pointing slightly forward
 tMatrix.addColumn(iHat);
 tMatrix.addColumn(jHat);
 tMatrix.addColumn(kHat);
 //create our cube matrix (Pseudo Grid)
-const cubeMatrix = new matrix();
+var cubeMatrix = new matrix();
 cubeMatrix.addColumn([0, 0, 0]);
 cubeMatrix.addColumn([1, 0, 0]);
 cubeMatrix.addColumn([1, 1, 0]);
@@ -138,82 +139,13 @@ cubeMatrix.addColumn([0, 1, 1]);
 //tMatrix.printMatrix();
 //cubeMatrix.printMatrix();
 //By multiplying the tMatrix and cubeMatrix, you get the coordinates of the cube on the physical graph
-const rMatrix = multiplyMatrixs(tMatrix, cubeMatrix);
+var rMatrix = multiplyMatrixs(tMatrix, cubeMatrix);
 //rMatrix.printMatrix();
 //loop through the columns, and plot the points with the scale transformation applied to them
-const scale = 100;
-for (let i = 0; i != rMatrix.width; i += 1) {
-    const points = rMatrix.getColumn(i);
+var scale = 100;
+for (var i = 0; i != rMatrix.width; i += 1) {
+    var points = rMatrix.getColumn(i);
     points[0] = points[0] * scale;
     points[1] = points[1] * scale;
     plotPoint(points);
 }
-/*
-//DEFINE OUR HAT VECTORS (X, Y, Z)
-//The hat vector is in the form [x, y], the physical direction is it pointing on the graph
-const xHat = [-1, -1]
-const yHat = [1, -1]
-const zHat = [0, 1] //the zHat is pointing backwards so it wouldn't show any thing on the graph
-
-const transformationMatrix = [
-    [xHat[0], yHat[0], zHat[0]],
-    [xHat[1], yHat[1], zHat[1]]
-//      x        y        z
-]
-
-
-//now lets create a basic 1 x 1 x 1 cube
-//Points are in form of [x, y, z], which is their position on the grid, we can consider this entire object 1 matrix if you flip it to its right
-const cubeVertexs = [
-    [0, 0, 0],
-    [1, 0, 0],
-    [1, 1, 0],
-    [0, 1, 0],
-
-    [0, 0, 1],
-    [1, 0, 1],
-    [1, 1, 1],
-    [0, 1, 1],
-]
-//To render this onto the page we just need to multiply each axis in each point, by the corresponding hat, and it should give us the correct position on the graph
-
-//Or we could multiply the transformation matrix by the cube matrix: [ x 3] * [3 x 8]
-const cubeVertexsMatrix = [ //flipping the above onto it's left to actually create the matrix
-    [0, 1, 1, 0,    0, 1, 1, 0], //x
-    [1, 1, 0, 0,    1, 1, 0, 0], //y
-    [1, 1, 1, 1,    0, 0, 0, 0], //z
-]
-
-//By multiplying both these matrix's (transformationMatrix * cubeVertexsMatrix) you could get: (each column of this represents a point on the graph)
-const resultMatrix = [
-    [1, 0, -1, 0, 1, 0, -1, 0], //x
-    [2, 1,  0, 1, 1, 0, -1, 0]  //y
-]
-
-
-//convert it into a plotable array
-const pointsToPlot = [
-    [1, 2],
-    [0, 1],
-    [-1, 0],
-    [0, 1],
-    [1, 1],
-    [0, 0],
-    [-1, -1],
-    [0, 0]
-]
-
-//now just plot these points
-for (i in pointsToPlot)
-{
-    const point = pointsToPlot[i];
-    const scale = 100;
-    const xOffset = 500;
-    const yOffset = 500;
-
-    point[0] = point[0] * scale + xOffset;
-    point[1] = point[1] * scale + yOffset;
-    console.log(point);
-    plotPoint(point);
-}
-*/ 
