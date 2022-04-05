ebaf580987a16f8e2bc4d04769f36407234b03e7
AryaaSk <aryaa_saravana@outlook.com>
2022-04-05 08:47:34 +0100

    added a third cube to the world, in future need to make a single render function which takes in all objects at once

--- JS/script.js
+++ JS/script.js
@@ -18,21 +18,34 @@ cube2.rotation = { x: 0, y: 0, z: 0 };
 cube2.scale = 1;
 cube2.position = { x: 0, y: 0, z: 300 };
 cube2.updateMatrices();
+const cube3 = new Box(100, 100, 100);
+cube3.position = { x: -200, y: 0, z: 0 };
+cube3.faceColours = {
+    "-z": "#5a16e0",
+    "-y": "#7a39fa",
+    "-x": "#40109e",
+    "+x": "#40109e",
+    "+y": "#7a39fa",
+    "+z": "#5a16e0",
+};
 const camera = new Camera();
 camera.position.x = 0;
 camera.position.y = 0;
-camera.worldRotation.x = 0;
-camera.worldRotation.y = 0;
+camera.worldRotation.x = -30;
+camera.worldRotation.y = 30;
 camera.worldRotation.z = 0;
 camera.updateRotationMatrix();
+//IN FUTURE I WILL CREATE A SINGLE CAMERA.RENDER() FUNCTION, WHERE YOU CAN PASS IN ALL THE ELEMENTS, AND IT WILL RENDER THEM IN THE CORRECT ORDER
+//YOU JUST NEED TO FIND THE CENTER OF EACH OBJECT, AND THEN CHECK WHICH IS CLOSESTS TO (0, 0, -50000), LIKE YOU DO INDIVIDUALLY FOR THE FACES
 setInterval(() => {
-    cube.rotation.x += 1;
-    cube.rotation.y += 1;
+    //cube.rotation.x += 1;
+    //cube.rotation.y += 1;
     cube.updateMatrices();
     clearCanvas();
     camera.renderGrid();
     camera.render(cube2);
     camera.render(cube);
+    camera.render(cube3);
     plotPoint([0, 0], "#000000"); //a visual marker of where it will zoom into
 });
 document.onkeydown = ($e) => {
