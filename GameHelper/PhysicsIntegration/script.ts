28494659b8d6223cecb3c8136d31b7a1188971a3
AryaaSk <aryaa_saravana@outlook.com>
2022-04-23 21:13:19 +0100

    moved syncObject() to gameHelper

--- GameHelper/PhysicsIntegration/script.ts
+++ GameHelper/PhysicsIntegration/script.ts
@@ -2,15 +2,6 @@
 //I'm not sure if the object's will match up to their cannon js counterparts
 
 //Functions
-const syncObject = (cannonBody: CANNON.Body, aryaa3DBody: Shape) => {
-    aryaa3DBody.position.x = cannonBody.position.x;
-    aryaa3DBody.position.y = cannonBody.position.y;
-    aryaa3DBody.position.z = cannonBody.position.z;
-
-    //to get rotation, we need to convert the quaternion, into XYZ Euler angles
-    aryaa3DBody.quaternion = { x: cannonBody.quaternion.x, y: cannonBody.quaternion.y, z: cannonBody.quaternion.z, w: cannonBody.quaternion.w };
-    aryaa3DBody.updateMatrices();
-} 
 const syncShapeRotation = ( parentShape: Shape, childShape: Shape ) => {
     childShape.quaternion = parentShape.quaternion;
     childShape.updateMatrices();
--- GameHelper/gameHelper.ts
+++ GameHelper/gameHelper.ts
@@ -20,3 +20,15 @@ const syncCamera = (camera: Camera, object: Shape) => { //Syncs camera to third 
     camera.worldRotation.y = -objectYRotation;
     camera.updateRotationMatrix();
 }
+
+
+//CannonJS Physics Functions, import cannonjs before using any of these
+const syncObject = (cannonBody: CANNON.Body, aryaa3DBody: Shape) => {
+    aryaa3DBody.position.x = cannonBody.position.x;
+    aryaa3DBody.position.y = cannonBody.position.y;
+    aryaa3DBody.position.z = cannonBody.position.z;
+
+    //to get rotation, we need to convert the quaternion, into XYZ Euler angles
+    aryaa3DBody.quaternion = { x: cannonBody.quaternion.x, y: cannonBody.quaternion.y, z: cannonBody.quaternion.z, w: cannonBody.quaternion.w };
+    aryaa3DBody.updateMatrices();
+} 
