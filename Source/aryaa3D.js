10847c3e7daa58b353208a844120e1c0d0f94117
AryaaSk <aryaa_saravana@outlook.com>
2022-05-03 19:37:09 +0100

    Started making progress on perspective

--- Source/aryaa3D.js
+++ Source/aryaa3D.js
@@ -600,10 +600,11 @@ class Camera {
     worldRotationMatrix = new matrix();
     showScreenOrigin = false;
     transformMatrix(points, objectPosition) {
-        let cameraObjectMatrix = points.copy();
-        cameraObjectMatrix.translateMatrix(objectPosition.x, objectPosition.y, objectPosition.z); //translate for object's position
-        cameraObjectMatrix.translateMatrix(-this.position.x, -this.position.y, -this.position.z); //translating relative to camera's position
-        cameraObjectMatrix = multiplyMatrixs(this.worldRotationMatrix, cameraObjectMatrix); //rotate for global world rotation
+        let worldPoints = points.copy(); //these are the points inside the world
+        worldPoints.translateMatrix(objectPosition.x, objectPosition.y, objectPosition.z); //translate for object's position
+        worldPoints.translateMatrix(-this.position.x, -this.position.y, -this.position.z); //translating relative to camera's position
+        worldPoints = multiplyMatrixs(this.worldRotationMatrix, worldPoints); //rotate for global world rotation
+        const cameraObjectMatrix = worldPoints.copy(); //these are the points after having perspective applied to them
         cameraObjectMatrix.translateMatrix(-this.absPosition.x, -this.absPosition.y, 0); //translate for absolute position
         cameraObjectMatrix.scaleUp(this.zoom); //scale for zoom
         return cameraObjectMatrix;
@@ -611,9 +612,8 @@ class Camera {
     render(objects) {
         const objectData = [];
         for (let objectIndex = 0; objectIndex != objects.length; objectIndex += 1) {
-            //transform the object's physicalMatrix to how the camera would see it:
             const object = objects[objectIndex];
-            const cameraObjectMatrix = this.transformMatrix(object.physicalMatrix, { x: object.position.x, y: object.position.y, z: object.position.z });
+            const cameraObjectMatrix = this.transformMatrix(object.physicalMatrix, object.position);
             //work out center of shape by finding average of all points
             let [totalX, totalY, totalZ] = [0, 0, 0];
             for (let i = 0; i != cameraObjectMatrix.width; i += 1) {
