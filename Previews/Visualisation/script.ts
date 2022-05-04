5d10df9bd2e29d84fcabb25433f4d8579276669b
AryaaSk <aryaa_saravana@outlook.com>
2022-05-04 21:02:47 +0100

    Moved visualisation into previews folder

---
+++ Previews/Visualisation/script.ts
@@ -0,0 +1,4 @@
+//basically just want to be able to add objects, and show the lines going into the camera
+//also want to show the point where they intersect the viewport (optional: draw lines on the viewport to show resulting image)
+
+//will be done in absolute mode since it is easier to understand and work with.
